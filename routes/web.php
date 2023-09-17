c45feaffc805eef7ca4c6718bbcb01d654dc6cee
Emil <emil8767_11@mail.ru>
2023-09-17 17:48:02 +0700

    add Dockerfile

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/debug-sentry', function () {
+    throw new Exception('My first Sentry error!');
+});
+
 require __DIR__ . '/auth.php';

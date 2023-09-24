0031a6b99feec765556245241544687429a26d94
Emil <emil8767_11@mail.ru>
2023-09-24 15:39:03 +0700

    set my domain name in the config/mail.php

--- config/mail.php
+++ config/mail.php
@@ -46,7 +46,7 @@ return [
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN'),
+            'local_domain' => env('MAIL_EHLO_DOMAIN', 'project-57-gwit.onrender.com'),
         ],
 
         'ses' => [
